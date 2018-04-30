6b8f92dd420d9611fe2ddddcc099933cf37cd9ad
sxysxy <sxysxygm@gmail.com>
2018-04-30 13:48:37 +0800

    there is some bug

--- STGFramework/HighLevelRenderer/Renderer2D.rb
+++ STGFramework/HighLevelRenderer/Renderer2D.rb
@@ -20,6 +20,7 @@ class Renderer2D < DX::RenderPipeline
 	
 		@sf = HFSF.loadsf_file($device, SHADERS_FILE)[0]
 		@quality = quality
+		
 		@sf.section[:basic].apply(self)
 		@sf.input_layout.apply(self)
 		set_target(Graphics.rtt)
@@ -30,9 +31,11 @@ class Renderer2D < DX::RenderPipeline
 	
 	def pre_draw_solid
 		@sf.section[:draw_solid].apply(self) if @phase != PHASE_DRAW_SOLID
+		@phase = PHASE_DRAW_SOLID
 	end
 	def pre_draw_wireframe
 		@sf.section[:draw_wireframe].apply(self) if @phase != PHASE_DRAW_WIREFRAME
+		@phase = PHASE_DRAW_WIREFRAME
 	end
 	def pre_draw_texture
 		return if @phase == PHASE_DRAW_TEXTURE
@@ -41,6 +44,7 @@ class Renderer2D < DX::RenderPipeline
 		else
 			@sf.section[:texturingLQ].apply(self)
 		end
+		@phase = PHASE_DRAW_TEXTURE
 	end
 	
 	def set_viewport(r)
@@ -73,6 +77,36 @@ class Renderer2D < DX::RenderPipeline
 				[Float(rect.x), Float(rect.y+rect.h), 0], [color.r, color.g, color.b, color.a],
 				[Float(rect.x+rect.w), Float(rect.y+rect.h), 0], [color.r, color.g, color.b, color.a]].flatten.pack("f*")
 		vb = DX::VertexBuffer.new($device, 7*4, 4, vecs)
+		set_topology(DX::TOPOLOGY_TRIANGLESTRIP)
+		set_vbuffer(vb)
+		draw(0, 4)
+		vb.release
+	end
+	
+	def draw_texture(texture, dest_rect, src_rect = nil)
+		pre_draw_texture
+		
+		rect = src_rect ? src_rect : HFRect(0, 0, texture.width, texture.height)
+		w = rect.w
+		h = rect.h
+		#LT
+		x1 = Float(rect.x) / w  
+		y1 = Float(rect.y) / h 
+		#RT
+		x2 = Float(rect.x + rect.w) / w
+		y2 = y1
+		#LB
+		x3 = x1
+		y3 = Float(rect.y + rect.h) / h
+		#RB
+		x4 = x2
+		y4 = y3
+		
+		vecs = [[Float(dest_rect.x), Float(dest_rect.y),             0], [x1, y1],
+				[Float(dest_rect.x+dest_rect.w), Float(dest_rect.y), 0], [x2, y2],
+				[Float(dest_rect.x), Float(dest_rect.x+dest_rect.h), 0], [x3, y3],
+				[Float(dest_rect.x+dest_rect.w), Float(dest_rect.y+dest_rect.h), 0],[x4, y4]].flatten.pack("f*")
+		vb = DX::VertexBuffer.new($device, 5*4, 4, vecs)
 		set_topology(DX::TOPOLOGY_TRIANGLESTRIP)
 		set_vbuffer(vb)
 		draw(0, 4)
--- STGFramework/HighLevelRenderer/Renderer2DShaders.rb
+++ STGFramework/HighLevelRenderer/Renderer2DShaders.rb
@@ -1,15 +1,7 @@
-Program("Renderer2D") {
+Program("Draw") {
 	Code(%{
 		cbuffer viewport : register(b0) {
 			float vp_x, vp_y, vp_w, vp_h;
-		};
-		struct VSTextureIn {
-			float4 position : POSITION;
-			float2 tex : TEXCOORD;
-		};
-		struct VSTextureOut {
-			float4 position : SV_POSITION;
-			float2 tex : TEXCOORD;
 		};
 		struct VSDrawIn {
 			float4 position : POSITION;
@@ -19,11 +11,6 @@ Program("Renderer2D") {
 			float4 position : SV_POSITION;
 			float4 color : COLOR;
 		};
-		
-		VSTextureOut VSTexture(VSTextureIn ipt) {
-			VSTextureOut opt = (VSTextureOut)ipt;
-			return opt;
-		}
 		VSDrawOut VSDraw(VSDrawIn ipt) {
 			VSDrawOut opt;
 			opt.position.x = ipt.position.x * 2.0 / vp_w - 1.0f;
@@ -33,12 +20,6 @@ Program("Renderer2D") {
 			opt.color = ipt.color;
 			return opt;
 		}
-		
-		SamplerState color_sampler : register(s0);
-		Texture2D color_map : register(t0);
-		float4 PS_Texture(VSTextureOut data) : SV_TARGET {
-			return color_map.Sample(color_sampler, data.tex);
-		}
 		float4 PS_Color(VSDrawOut data) : SV_TARGET {
 			return data.color;
 		}
@@ -46,19 +27,8 @@ Program("Renderer2D") {
 	InputLayout {
 		Format "POSITION", DX::R32G32B32_FLOAT
 		Format "COLOR", DX::R32G32B32A32_FLOAT
-		Format "TEXCOORD", DX::R32G32_FLOAT
 	}
 	Resource {
-		Sampler("LQSampler") {
-			use_default
-			set_filter DX::FILTER_MIN_MAG_MIP_POINT, 0
-		}
-		Sampler("HQSampler") {
-			use_default
-		}
-		Blender("blender") {
-			use_default
-		}
 		Rasterizer("RS_fill_solid") {
 			use_default
 			set_cull_mode DX::CULL_NONE
@@ -89,14 +59,82 @@ Program("Renderer2D") {
 		#set_ps_sampler(0, nil)
 		set_rasterizer("RS_fill_wireframe")
 	}
+
+}
+
+Program("Texture2D") {
+	Code(%{
+		cbuffer viewport : register(b0) {
+			float vp_x, vp_y, vp_w, vp_h;
+		}
+		cbuffer angle_transform : register(b1) {
+			float angle;
+			int vmirror, hmirror;			//Vertical Mirror & Horizon Mirror
+			int emm; //unused
+		}
+		struct VSTextureIn {
+			float4 position : POSITION;
+			float2 tex : TEXCOORD;
+		};
+		struct VSTextureOut {
+			float4 position : SV_POSITION;
+			float2 tex : TEXCOORD;
+		};
+		VSTextureOut VSTexture(VSTextureIn ipt) {
+			VSTextureOut opt;
+			float x1 = ipt.position.x * 2.0 / vp_w - 1.0f;
+			float y1 = -(ipt.position.y * 2.0 / vp_h) + 1.0f;
+			opt.position.x = x1 * cos(angle) - y1 * sin(angle);
+			opt.position.y = x1 * sin(angle) + y1 * cos(angle);
+			opt.position.z = ipt.position.z;
+			opt.position.w = 1.0f;
+			opt.tex = ipt.tex;
+			return opt;
+		}
+		SamplerState color_sampler : register(s0);
+		Texture2D color_map : register(t0);
+		float4 PS_Texture(VSTextureOut data) : SV_TARGET {
+			return color_map.Sample(color_sampler, data.tex);
+		}
+	})
+	
+	InputLayout {
+		Format "POSITION", DX::R32G32B32_FLOAT
+		Format "TEXCOORD", DX::R32G32_FLOAT
+	}
+	Resource {
+		Sampler("LQSampler") {
+			use_default
+			set_filter DX::FILTER_MIN_MAG_MIP_POINT, 0
+		}
+		Sampler("HQSampler") {
+			use_default
+		}
+		Blender("blender") {
+			use_default
+		}
+		ConstantBuffer("angle_transform") {
+			set_size 16
+			set_init_data [0.0, 0, 0, 0].pack("fi*")
+		}
+		ConstantBuffer("viewport") {
+			set_size 16
+		}
+	}
+	Section("basic") {
+		set_vshader("VSTexture")
+		set_vs_cbuffer(0, "viewport")
+		set_vs_cbuffer(1, "angle_transform")
+		set_blender("blender")
+	}
 	Section("texturingHQ") {
-		set_vshader("VSTexture");
 		set_pshader("PS_Texture")
 		set_ps_sampler(0, "HQSampler")
 	}
 	Section("texturingLQ") {
 		set_pshader("PS_Texture")
-		set_vshader("VSTexture");
 		set_ps_sampler(0, "LQSampler")
 	}
+	
 }
+
