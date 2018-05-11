301a6587aed4d20361171c24da9edbc1407a5086
sxysxy <sxysxygm@gmail.com>
2018-05-11 18:46:33 +0800

    s

--- STGFramework/STGScene/SceneSHWStage.rb
+++ STGFramework/STGScene/SceneSHWStage.rb
@@ -9,17 +9,21 @@ class SceneSHWStage < SceneStage
 	
 	def init_back
 		@sp_panel = TextureCache.load("./stage/back.png")
+		
 		Graphics.re.insert(@rd_back, 200)
 	end
 	def init_content
 		@reimus = TextureCache.load("/th14/player/pl00/pl00.png")
 		
 		@reimu_forward, @reimu_left, @reimu_right, @reimu_back = Array.new(4) {DX::Texture2D.new($device, 32, 48)}
+		
+		Graphics.re.lock
 		@rd_content.set_target(DX::RTT.new(@reimu_forward))
 		@rd_content.draw_texture(@reimus, HFRect(0, 0, 32, 48), HFRect(32, 0, 32, 48))
-		Graphics.re.lock
 		@rd_content.immdiate_render
+		@rd_content.use_default_target
 		Graphics.re.unlock
+		
 		Graphics.re.insert(@rd_content, 100)
 	end
 	
@@ -33,11 +37,14 @@ class SceneSHWStage < SceneStage
 	def draw_background
 		super
 		@rd_back.draw_texture(@sp_panel, HFRect(0, 0, @sp_panel.width, @sp_panel.height))
-		@rd_back.draw_texture(@reimius, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
 	end
 	
+	derive(:draw_content)
 	def draw_content
-		#@rd_content.draw_texture(@reimu_forward, HFRect(100, 100, 32, 48))
-		#@rd_content.draw_texture(@reimius, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
+		#@rd_content.set_target(DX::RTT.new(@reimu_forward))
+		#@rd_content.draw_texture(@reimus, HFRect(0, 0, 32, 48), HFRect(32, 0, 32, 48))
+		#@rd_content.use_default_target
+		#@rd_content.draw_texture(@reimu_forward, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
+		@rd_content.draw_texture(@reimus, HFRect(0, 0, @reimus.width, @reimus.height))
 	end
 end
