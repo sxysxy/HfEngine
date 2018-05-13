04de589cd2c1eaec29e7709b160452990b6d0d73
sxysxy <sxysxygm@gmail.com>
2018-05-13 18:42:59 +0800

    It is a bad Renderer2D.rb...

--- STGFramework/STGLogic/LogicStage.rb
+++ STGFramework/STGLogic/LogicStage.rb
@@ -1,8 +1,17 @@
 require_relative "../SceneManager.rb"
 class LogicStage
 
+	attr_reader :player_x, :player_y #center pos
+	PLAYER_W = 32
+	PLAYER_H = 48
+	attr_reader :player_dir
+
 	def initialize(s)
 		@scene = s
+		
+		@player_x = 200 - PLAYER_H/2
+		@player_y = 212 - PLAYER_W/2
+		@player_dir = 8 #as numpad 8↑ 2↓ 4←，etc
 	end
 
 	def update
--- STGFramework/STGScene/SceneSHWStage.rb
+++ STGFramework/STGScene/SceneSHWStage.rb
@@ -1,10 +1,13 @@
 require_relative "../CommonScene/SceneStage.rb"
+require_relative '../STGLogic/LogicStage.rb'
 
 class SceneSHWStage < SceneStage
 	def start
 		super
 		init_back
 		init_content
+		
+		@logic = LogicStage.new(self)
 	end
 	
 	def init_back
@@ -14,23 +17,18 @@ class SceneSHWStage < SceneStage
 	end
 	def init_content
 		@reimus = TextureCache.load("/th14/player/pl00/pl00.png")
-		
-		@reimu_forward, @reimu_left, @reimu_right, @reimu_back = Array.new(4) {DX::Texture2D.new($device, 32, 48)}
-=begin
-		Graphics.re.lock
-		@rd_content.set_target(DX::RTT.new(@reimu_forward))
-		@rd_content.draw_texture(@reimus, HFRect(0, 0, 32, 48), HFRect(32, 0, 32, 48))
-		@rd_content.immdiate_render
-		@rd_content.use_default_target
-		Graphics.re.unlock
-=end
+		@rd_content.set_viewport(HFRect(25, 25, 400, 425))
 		Graphics.re.insert(@rd_content, 100)
+	end
+	
+	def update
+		@logic.update
+		super
 	end
 	
 	def terminate
 		super
 		Graphics.re.clear
-		[@reimu_forward, @reimu_back, @reimu_left, @reimu_right].each &:release
 	end
 	
 	derive(:draw_background) 
@@ -41,16 +39,6 @@ class SceneSHWStage < SceneStage
 	
 	derive(:draw_content)
 	def draw_content
-		#@rd_content.set_target(DX::RTT.new(@reimu_forward))
-		#@rd_content.draw_texture(@reimus, HFRect(0, 0, 32, 48), HFRect(32, 0, 32, 48))
-		#@rd_content.use_default_target
-		#@rd_content.draw_texture(@reimu_forward, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
-		#@rd_content.draw_texture(@reimus, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
-		@rd_content.set_target(DX::RTT.new(@reimu_forward))
-		@rd_content.set_viewport(HFRect(0, 0, @reimu_forward.width, @reimu_forward.height))
-		@rd_content.draw_texture(@reimus, HFRect(0, 0, 32, 48), HFRect(32, 0, 32, 48))
-		@rd_content.use_default_target
-		@rd_content.set_viewport(HFRect(0, 0, $window.width, $window.height))
-		@rd_content.draw_texture(@reimu_forward, HFRect(100, 100, 32, 48))
+		@rd_content.draw_texture(@reimus, HFRect(100, 100, 32, 48), HFRect(0, 0, 32, 48))
 	end
 end
