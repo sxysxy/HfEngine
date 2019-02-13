9a12b1abef8251fc201de395fbe5c2b1a34a4714
Xiangyu Shi <sxysxygm@gmail.com>
2019-02-14 01:32:24 +0800

    Turn to ruby2.6

--- HfEngine/Include/Utility/embeddedruby.h
+++ HfEngine/Include/Utility/embeddedruby.h
@@ -2,16 +2,16 @@
 
 #ifdef _WIN64
 
-#include "C:/ruby250-x64-mswin64/include/ruby-2.5.0/ruby.h"
-#pragma comment(lib, "C:/ruby250-x64-mswin64/lib/x64-vcruntime140-ruby250.lib")
+#include "../../../ThirdParty/ruby260-x64-mswin64/include/ruby-2.6.0/ruby.h"
+#pragma comment(lib, "x64-vcruntime140-ruby260.lib")
 
 #define FIX2PTR(v) NUM2ULL(v) 
 
 #else
 
-#include "C:/ruby250-mswin32/include/ruby-2.5.0/ruby.h"
-#pragma comment(lib, "C:/ruby250-mswin32/lib/vcruntime140-ruby250.lib")
-//fclose is not only in msvcrt, but also in vcruntime140-ruby250...
+#include "../../../ThirdParty/ruby260-mswin32/include/ruby-2.6.0/ruby.h"
+#pragma comment(lib, "vcruntime140-ruby260.lib")
+//fclose is not only in msvcrt, but also in ruby's binary file.
 
 #define FIX2PTR(x) FIX2ULONG(x)
 
